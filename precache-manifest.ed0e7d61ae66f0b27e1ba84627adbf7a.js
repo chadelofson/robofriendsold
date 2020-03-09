bd4763e4aa925aceace7383c2ea287da830bd8a2
chadelofson <chad.elofson@gmail.com>
2020-03-08 17:33:12 -0700

    Updates

---
+++ precache-manifest.ed0e7d61ae66f0b27e1ba84627adbf7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5975a8e33528b071124ea84c657e1c70",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5f535368eb9666fcd4f9",
+    "url": "/robofriends/static/css/main.68c3906f.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f535368eb9666fcd4f9",
+    "url": "/robofriends/static/js/main.71d96075.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  },
+  {
+    "revision": "3f5c9111609279eb69be9e1a8a10e82b",
+    "url": "/robofriends/static/media/SEGA-LOGO-FONT.ttf.3f5c9111.woff"
+  },
+  {
+    "revision": "57ad6cb1828e85f5f029d166ac0361b8",
+    "url": "/robofriends/static/media/SEGA-LOGO-FONT.ttf.57ad6cb1.eot"
+  },
+  {
+    "revision": "d7d67c411a56f01384056ff8a85eb4c6",
+    "url": "/robofriends/static/media/SEGA-LOGO-FONT.ttf.d7d67c41.svg"
+  }
+]);
